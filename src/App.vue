e807a906e8cc84c89247883ae33d3609611e71b7
Predrag Markovljev <predrag.markovljev@gmail.com>
2022-12-30 19:23:47 +0100

    Added task-list template inside App.vue; Created TaskDetails.vue component; Added CSS styling for TaskDetails.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTaskStore } from "./stores/TaskStore";
+import TaskDetails from "@/components/TaskDetails.vue";
+
+/// This returns us the 'store' property that we are placing inside the 'taskStore' constant
+const taskStore = useTaskStore();
+// console.log(taskStore.tasks[0]); // temp
+</script>
 
 <template>
   <main>
@@ -7,6 +14,12 @@
       <img src="@/assets/pinia-logo.svg" alt="Pinia Logo" />
       <h1>Pinia Tasks</h1>
     </header>
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <task-details :task="task"/>
+      </div>
+    </div>
   </main>
 </template>
 
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type Task from "@/types/Task";
+import { defineProps, type PropType } from "vue";
+
+const props = defineProps({
+  task: {
+    required: true,
+    type: Object as PropType<Task>,
+  },
+});
+</script>
+
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <!-- Inside index.html we created a linked to material icons -->
+    <!-- Icon for trash can is 'delete' -->
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
